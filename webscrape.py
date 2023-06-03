7306eb308c72ec75aa4d77bac844ce0156578a7d
Ge <55168978+GeSamaras@users.noreply.github.com>
2023-06-03 03:33:52 -0300

    Update webscrape.py

--- webscrape.py
+++ webscrape.py
@@ -1,10 +1,34 @@
+# scraping using selenium 
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 import pandas as pd
 
-df_csvtable = pd.read_csv('https://www.football-data.co.uk/mmz4281/2122/E0.csv') #pd.read_html to get html as input
-print('df_csvtable')
+web = ' ' #link to the page
+path = '/Users/'  # introduce path here
 
-df_csvtable = df_csvtable.rename(columns={'Date':'date',
-                                            'HomeTeam':'home_team',
-                                            'AwayTeam':'away_team',
-                                            'FTHG': 'home_goals',
-                                            'FTAG': 'away_goals'})
+# Creating the driver
+driver_service = Service(executable_path=path)
+driver = webdriver.Chrome(service=driver_service)
+driver.get(web)
+
+# Finding Elements
+containers = driver.find_elements(by='xpath', value='//div[@class="teaser__copy-container"]')
+
+titles = []
+subtitles = []
+links = []
+for container in containers:
+    title = container.find_element(by='xpath', value='./a/h2').text
+    subtitle = container.find_element(by='xpath', value='./a/p').text
+    link = container.find_element(by='xpath', value='./a').get_attribute('href')
+    titles.append(title)
+    subtitles.append(subtitle)
+    links.append(link)
+
+# Exporting data to a CSV file
+my_dict = {'title': titles, 'subtitle': subtitles, 'link': links}
+df_headlines = pd.DataFrame(my_dict)
+df_headlines.to_csv('headline.csv')
+
+driver.quit()
