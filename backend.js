55f3f1ce8af1e5b1d5bab7a96c2337779954f0b9
Ge <55168978+GeSamaras@users.noreply.github.com>
2023-06-06 04:49:20 -0300

    Create backend.js

---
+++ backend.js
@@ -0,0 +1,11 @@
+exports.handler = async (event) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ message: 'Hello, World!' }),
+  };
+
+  return response;
+};
